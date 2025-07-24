04c9f6c63931997326b45e173d8af4392c7d4eee
Mehmood Khalil <mehmoodkhalil@gmail.com>
2025-07-24 18:54:17 +0500

    Javascript Started

---
+++ Javascript/28-Lecture/custom.js
@@ -0,0 +1,21 @@
+var subject = "Web Programming";
+var engMarks = 80;
+var compMarks = 65;
+var mathMarks = 76;
+var physicsMarks = 66;
+var chemistryMarks = 78;
+var urduMarks = 34;
+
+var obtMarks = engMarks + compMarks + mathMarks + physicsMarks + chemistryMarks + urduMarks;
+var perc = (obtMarks/600) * 100;
+
+alert("You got " + obtMarks + "marks and your percentage is " + perc + "%");
+// alert(perc);
+alert(5*5)
+alert(20+40)
+
+// alert(subject)
+
+// Assignment
+// All Arthimetic Operations using console and Alert with the help of variables
+
